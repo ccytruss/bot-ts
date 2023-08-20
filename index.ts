d3324eb3ba387fb315e96f3a344b46c9980cc5cc
ccytruss <99399244+ccytruss@users.noreply.github.com>
2023-08-20 23:04:51 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
+import WebSocket from "ws"
+
+interface User {
+    world: string,
+    chash: string,
+    hs3: string,
+    mchar_id: number,
+    user_id: number,
+}
+
+interface Instance {
+    browser_token: string;
+    ev: number;
+}
+
 class socketHandler {
     get: URLSearchParams;
+    instance: Instance;
+    dataToSend: string;
 
-    constructor() {
+    constructor(instance: Instance, dataToSend: string = "") {
         this.get = new URLSearchParams();
+        this.instance = instance;
+        this.dataToSend = dataToSend;
     }
 
     addData(key: string, value: string) {
@@ -13,40 +32,78 @@ class socketHandler {
         }
     }
 
-    handleMessages(event: MessageEvent) {
+    handleMessages(data: WebSocket.Data) {
         try {
-            const message = JSON.parse(event.data);
-            console.log(message);
+            console.log(data);
         } catch (error) {
             console.log(error);
         }
     }
-
+    
     construct() {
-
+        if (this.instance.ev) {
+            this.addData("ev", this.instance.ev.toString());
+        }
+        if (this.instance.browser_token) {
+            this.addData("browser_token", this.instance.browser_token);
+        }
+        return { g: decodeURIComponent(this.get.toString()), p: this.dataToSend };
     }
 }
 
 class Engine {
-    browser_token?: string;
-    ev?: number;
     socketHandler: socketHandler;
+    user: User;
+    instance: Instance;
 
-    constructor() {
-        this.socketHandler = new socketHandler();
+    constructor(user: User) {
+        this.instance = {
+            browser_token: "",
+            ev: 0
+        };
+        this.socketHandler = new socketHandler(this.instance);
+        this.user = user;
     }
 
     connect() {
-        const ws = new WebSocket("");
-        ws.onopen = () => {
+        const cookieString = Object.entries(this.user)
+            .map(([key, value]) => `${key}=${value}`)
+            .join('; ');
 
+        const options = {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+                "Origin": `https://${this.user.world}.margonem.pl`,
+                "Referer": `https://${this.user.world}.margonem.pl`,
+                "Host": `${this.user.world}.margonem.pl`,
+                "Cookie": cookieString
+            }
+        };
+
+        const engine = new WebSocket(`wss://${this.user.world}.margonem.pl/ws-engine`, options);
+
+        engine.onopen = () => {
+            for (let init = 1; init < 5; init++) {
+                this.socketHandler.addData('t', init < 5 ? 'init' : '_');
+                this.socketHandler.addData('initlvl', init.toString());
+                this.socketHandler.addData('clientTs', (Date.now() / 1000).toString());
+                this.socketHandler.addData('mucka', Math.random().toString());
+                const initData = this.socketHandler.construct()
+                console.log(initData)
+                engine.send(JSON.stringify(initData))
+            }
         }
-        ws.onmessage = this.socketHandler.handleMessages.bind(this.socketHandler);
-        
-        for (let i = 0; i < 5; i++) {
-            console.log(`init=${i}`)
-        }
+
+        engine.on('message', (data: WebSocket.Data) => {
+            this.socketHandler.handleMessages(data);
+        });
     }
 }
 
-new Engine().connect();
+new Engine({
+    world: "exampleWorld",
+    chash: "exampleChash",
+    hs3: "exampleHs3",
+    mchar_id: 1,
+    user_id: 1
+}).connect();
